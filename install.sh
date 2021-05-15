751b9f85b878abd5af809663504ea1dae13b7acf
im karthikeyan.v (cyber._.spartan) <33289300+karthi-the-hacker@users.noreply.github.com>
2021-05-15 13:20:34 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install sys
+python -m pip install sys
+pip3 install sys
+python -m pip3 install sys
+pip install requests
+python -m pip install requests
+pip3 install requests
+python -m pip3 install requests
